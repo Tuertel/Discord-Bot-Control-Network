c7899458e1df8d35e9444d886a72957d56946449
Tuertle <ricnek2222@gmail.com>
2021-01-27 22:20:09 +0100

    mostly finisched Bot Stats Formater

--- Control Network Lib/src/BotStatsFormater.cs
+++ Control Network Lib/src/BotStatsFormater.cs
@@ -4,12 +4,33 @@ using System.Text;
 
 namespace Control_Network_Lib.src
 {
-    extern class BotStatsFormater
+    public class BotStatsFormater
     {
-        
+        public static ShardFormater shardFormater = shardFormating;
+        public static BackEnd_RAMFormater backEnd_RAMFormater;
+        public static BackEnd_CPUFormater backEnd_CPUFormater;
+        public static BackEnd_averagePing backEnd_AveragePing = backEnd_ping;
+
         public delegate string ShardFormater(int onlineShards, int expectedShards);
         public delegate string BackEnd_RAMFormater();
         public delegate string BackEnd_CPUFormater();
         public delegate string BackEnd_averagePing(int[] pings);
+
+        private static string shardFormating(int onlineShards, int expectedShards)
+        {
+            return $"{onlineShards}/{expectedShards} ({expectedShards / 100 * onlineShards})";
+        }
+
+        private static string backEnd_ping(int[] pings)
+        {
+            int allpings = 0;
+
+            foreach (int p in pings)
+            {
+                allpings += p;
+            }
+
+            return $"{allpings / pings.Length}";
+        }
     }
 }
